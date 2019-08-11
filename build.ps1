b8c8dd8a693e904640c95cfa74e95f6ccca35d0c
Andreas Gruenwald <andreas@grynwald.net>
2019-08-11 20:14:31 +0200

    Add build.ps1

---
+++ build.ps1
@@ -0,0 +1,44 @@
+# Assumes "wyam" (https://wyam.io/) is installed either globally or in the same directory as this script
+
+function Log-Error($message) {
+    Write-Host "ERROR: $message"
+}
+function Log-Info($message) {
+    Write-Host "INFO: $message"
+}
+
+function Get-WyamPath {
+
+    Push-Location $PSScriptRoot
+    
+    Log-Info "Attempting to find Wyam installation in current directory"
+    $wyamCommand = Get-Command ".\wyam" -CommandType Application -ErrorAction SilentlyContinue
+    
+    if($wyamCommand -eq $null) {
+        Log-Info "No local installation of wyam found, attempting to find global installation"
+        $wyamCommand = Get-Command "wyam" -CommandType Application -ErrorAction SilentlyContinue
+    }
+        
+    if($wyamCommand -eq $null) {
+        Log-Error "Failed to locate wyam. Aborting."
+        throw "Build failed."
+    } else {
+        Log-Info "wyam found at $($wyamCommand.Source)"
+        return $wyamCommand.Source
+    }
+    
+    Pop-Location    
+}    
+
+$wyamPath = Get-WyamPath
+$inputRoot = $PSScriptRoot 
+
+Push-Location $inputRoot
+
+Log-Info "Starting wyam build"
+Invoke-Expression "$wyamPath build"
+if($LASTEXITCODE -ne 0) {
+    Log-Error "wyam failed with exit code $LASTEXITCODE"
+    throw "Build failed."
+}
+Pop-Location
